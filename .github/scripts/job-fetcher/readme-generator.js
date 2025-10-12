9c9217734bc33823a288e617fc39ed2391b3d24a
taha-mehmood-03 <132851626+taha-mehmood-03@users.noreply.github.com>
2025-10-12 12:02:53 -0800

    Update readme-generator.js

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -272,7 +272,7 @@ Connect with fellow job seekers, get career advice, share experiences, and stay 
 
  <div align="center">
   <a href="https://discord.gg/yKWw28q7Yq" target="_blank">
-    <img src="./discord-button.png"  alt="Join Discord - Job Finder & Career Hub by Zapply">
+    <img src="./discord-button.png" width="400" height="60"  alt="Join Discord - Job Finder & Career Hub by Zapply">
   </a>
 </div>
 
